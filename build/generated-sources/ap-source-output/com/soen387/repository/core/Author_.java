613309641fe60e8d683b5fbd0f17d6525e31eaed
Louis-Simon <lscarle07@gmail.com>
2019-10-26 16:25:48 -0400

    Cleaned Build + web.xml

---
+++ build/generated-sources/ap-source-output/com/soen387/repository/core/Author_.java
@@ -0,0 +1,15 @@
+package com.soen387.repository.core;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-10-26T16:13:01")
+@StaticMetamodel(Author.class)
+public class Author_ { 
+
+    public static volatile SingularAttribute<Author, String> author_fname;
+    public static volatile SingularAttribute<Author, String> author_lname;
+    public static volatile SingularAttribute<Author, Long> id;
+
+}
